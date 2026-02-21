218779794afa30ff1b6cbe61306532a27c10fefa
MrLad01 <ladayodeji@gmail.com>
2026-02-21 11:22:57 +0100

    update

--- app/football/page.tsx
+++ app/football/page.tsx
@@ -23,7 +23,7 @@ export default function Football() {
   const [currentVideo, setCurrentVideo] = useState(0);
 
   const videos = [
-    '/videos/fball-1-1.mp4',
+    '/videos/Fball-1-1.mp4',
     '/videos/fball-2-1.mp4',
     '/videos/pitch-1-1.mp4'
   ];
